0d05b432f5919be5b578b2bac6fb37df389fc9c4
Marc Sibson <sibson@gmail.com>
2016-08-08 09:37:17 -0700

    updates to keep it working and make easier to use

--- listflow.py
+++ listflow.py
@@ -6,18 +6,31 @@ from trello.util import create_oauth_token
 from github3 import login
 
 
-# https://trello.com/app-key
-TRELLO_API_KEY = os.environ['TRELLO_API_KEY']
-TRELLO_API_SECRET = os.environ['TRELLO_API_SECRET']
-TRELLO_TOKEN = os.environ.get('TRELLO_TOKEN') or create_oauth_token(expiration='1day', scope='read')
+class ConfigurationError(Exception):
+    pass
 
-GITHUB_USER = os.environ['GITHUB_USER']
-GITHUB_PASSWORD = os.environ['GITHUB_PASSWORD']
+TRELLO_API_KEY = os.environ.get('TRELLO_API_KEY')
+if not TRELLO_API_KEY:
+    raise ConfigurationError('Get a API KEY from https://trello.com/app-key')
+
+TRELLO_API_SECRET = os.environ.get('TRELLO_API_SECRET')
+if not TRELLO_API_SECRET:
+    raise ConfigurationError('Get an OAUTH SECRET from https://trello.com/app-key')
+
+TRELLO_API_TOKEN = os.environ.get('TRELLO_API_TOKEN')
+if not TRELLO_API_TOKEN:
+    create_oauth_token(expiration='1day', scope='read')
+
+
+GITHUB_USER = os.environ.get('GITHUB_USER')
+GITHUB_PASSWORD = os.environ.get('GITHUB_PASSWORD')
+if not GITHUB_USER or not GITHUB_PASSWORD:
+    raise ConfigurationError('Missing GitHub credentials, https://github.com/settings/tokens')
 
 
 def get_board(trello, board_name):
     for b in trello.list_boards():
-        if b.name == board_name:
+        if b.name == board_name or b.url.endswith(board_name):
             return b
 
 
@@ -28,7 +41,7 @@ def get_list(trello, board, list_name):
 
 
 def get_milestone(repo, title):
-    for m in repo.iter_milestones(state='open'):
+    for m in repo.milestones(state='open'):
         if m.title == title:
             return m
 
@@ -40,41 +53,57 @@ def get_issue(repo, title, milestone=None):
 
 
 def create_milestone_for_card(repo, card, labels=None):
-    print 'Building Milestone', card.name
+    print 'Building Milestone:', card.name
     milestone = get_milestone(repo, card.name)
+
     if milestone is None:
         description = '# [{}]({})\n'.format(card.name, card.short_url) + card.description
         milestone = repo.create_milestone(card.name, description=description)
 
-    card.fetch()  # need to fetch otherwise fetch_checklists will fail
     for cl in card.fetch_checklists():
         for cli in cl.items:
-            if get_issue(repo, card.name, milestone=milestone):
+            if get_issue(repo, card.name, milestone=card.name):
                 continue
 
-            print 'Creating Issue', cli['name']
-            description = '# [{}]({})'.format(card.name, card.short_url)
-            repo.import_issue(cli['name'], description, card.create_date, milestone=milestone.number, labels=labels)
+            print '\t - ', cli['name']
+            try:
+                created_at = card.create_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+            except IndexError:
+                created_at = None
 
+            description = '# [{}]({})'.format(card.name, card.short_url)
+            repo.import_issue(cli['name'], description, created_at, milestone=milestone.number, labels=labels)
+
+    print ''
     return milestone
 
 
 def convert_list(repo_name, board_name, list_name, labels=None):
-    trello = TrelloClient(TRELLO_API_KEY, token=TRELLO_TOKEN)
+    trello = TrelloClient(TRELLO_API_KEY, token=TRELLO_API_TOKEN)
 
     board = get_board(trello, board_name)
     tlist = get_list(trello, board, list_name)
 
-    repo = login(GITHUB_USER, GITHUB_PASSWORD).repository(repo_name.split('/', 1))
+    repo = login(GITHUB_USER, GITHUB_PASSWORD).repository(*repo_name.split('/', 1))
     for card in tlist.list_cards():
+        card.fetch()
         create_milestone_for_card(repo, card, labels=labels)
 
 
+def usage():
+    print 'listflow board list user/repo [labels]'
+
+
 def main():
-    repo_name = sys.argv[0]
+    if len(sys.argv) < 4:
+        usage()
+        sys.exit(1)
+
     board_name = sys.argv[1]
     list_name = sys.argv[2]
-    labels = sys.argv[3:]
+    repo_name = sys.argv[3]
+    labels = sys.argv[4:]
+
     convert_list(repo_name, board_name, list_name, labels)
 
 
