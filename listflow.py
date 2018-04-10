30a991532d1875c0bd8a60a488492870c155f9a5
Marc Sibson <sibson@gmail.com>
2018-04-10 15:09:19 -0400

    py3 support

--- listflow.py
+++ listflow.py
@@ -6,16 +6,28 @@ from trello.util import create_oauth_token
 from github3 import login
 
 
-class ConfigurationError(Exception):
-    pass
+class ListFlowError(Exception):
+    """ Base Exception for listflow """
+
+class ConfigurationError(RuntimeError):
+    """ Missing environment settings """
+
+class BoardNotFound(ListFlowError):
+    """ No matching boards found """
+
+class ListNotFound(ListFlowError):
+    """ No matching Lists found """
+
 
 TRELLO_API_KEY = os.environ.get('TRELLO_API_KEY')
 if not TRELLO_API_KEY:
-    raise ConfigurationError('Get a API KEY from https://trello.com/app-key')
+    print('Set TRELLO_API_KEY, from https://trello.com/app-key')
+    sys.exit(1)
 
 TRELLO_API_SECRET = os.environ.get('TRELLO_API_SECRET')
 if not TRELLO_API_SECRET:
-    raise ConfigurationError('Get an OAUTH SECRET from https://trello.com/app-key')
+    print('Set TRELLO_API_SECRET from the bottom of https://trello.com/app-key')
+    sys.exit(1)
 
 TRELLO_API_TOKEN = os.environ.get('TRELLO_API_TOKEN')
 if not TRELLO_API_TOKEN:
@@ -25,7 +37,8 @@ if not TRELLO_API_TOKEN:
 GITHUB_USER = os.environ.get('GITHUB_USER')
 GITHUB_PASSWORD = os.environ.get('GITHUB_PASSWORD')
 if not GITHUB_USER or not GITHUB_PASSWORD:
-    raise ConfigurationError('Missing GitHub credentials, https://github.com/settings/tokens')
+    print('Missing GitHub credentials, https://github.com/settings/tokens')
+    sys.exit(1)
 
 
 def get_board(trello, board_name):
@@ -33,12 +46,16 @@ def get_board(trello, board_name):
         if b.name == board_name or b.url.endswith(board_name):
             return b
 
+    raise BoardNotFound(board_name)
 
 def get_list(trello, board, list_name):
     for l in board.open_lists():
+        print(l, l.name, list_name)
+
         if l.name == list_name:
             return l
 
+    raise ListNotFound(list_name)
 
 def get_milestone(repo, title):
     for m in repo.milestones(state='open'):
@@ -53,7 +70,7 @@ def get_issue(repo, title, milestone=None):
 
 
 def create_milestone_for_card(repo, card, labels=None):
-    print 'Building Milestone:', card.name
+    print('Building Milestone:', card.name)
     milestone = get_milestone(repo, card.name)
 
     if milestone is None:
@@ -62,50 +79,78 @@ def create_milestone_for_card(repo, card, labels=None):
 
     for cl in card.fetch_checklists():
         for cli in cl.items:
-            if get_issue(repo, card.name, milestone=card.name):
+            name = cli['name']
+            issue = get_issue(repo, name, milestone=card.name)
+            if issue and 'https://github.com' not in name:
+                print('\t - {} linking to Github'.format(name))
+                cl.rename_checklist_item(name, '{} {}'.format(name, issue.html_url))
                 continue
 
-            print '\t - ', cli['name']
+            print('\t - ', name)
             try:
                 created_at = card.create_date.strftime('%Y-%m-%dT%H:%M:%SZ')
             except IndexError:
                 created_at = None
 
             description = '# [{}]({})'.format(card.name, card.short_url)
-            repo.import_issue(cli['name'], description, created_at, milestone=milestone.number, labels=labels)
+            issue = repo.import_issue(cli['name'], description, created_at, milestone=milestone.number, labels=labels)
 
-    print ''
+            cl.rename_checklist_item(name, '{} {}'.format(name, issue.html_url))
+    print('')
     return milestone
 
 
-def convert_list(repo_name, board_name, list_name, labels=None):
+def connect(repo_name):
     trello = TrelloClient(TRELLO_API_KEY, token=TRELLO_API_TOKEN)
+    repo = login(GITHUB_USER, GITHUB_PASSWORD).repository(*repo_name.split('/', 1))
+
+    return trello, repo
+
+
+def convert_list(repo_name, board_name, list_name, labels=None):
+    trello, repo = connect(repo_name)
 
     board = get_board(trello, board_name)
     tlist = get_list(trello, board, list_name)
 
-    repo = login(GITHUB_USER, GITHUB_PASSWORD).repository(*repo_name.split('/', 1))
     for card in tlist.list_cards():
         card.fetch()
         create_milestone_for_card(repo, card, labels=labels)
 
 
-def usage():
-    print 'listflow board list user/repo [labels]'
+def convert_card(repo_name, card, labels=None):
+    trello, repo = connect(repo_name)
+    card.fetch()
+    create_milestone_for_card(repo, card, labels=labels)
 
 
-def main():
-    if len(sys.argv) < 4:
-        usage()
+def listflow(*args):
+    """
+        listflow board list user/repo [labels]
+    """
+    if len(args) < 4:
+        print(args)
+        print(listflow.__doc__)
         sys.exit(1)
 
-    board_name = sys.argv[1]
-    list_name = sys.argv[2]
-    repo_name = sys.argv[3]
-    labels = sys.argv[4:]
+    board_name = args[0]
+    list_name = args[1]
+    repo_name = args[2]
+    labels = args[3:]
 
     convert_list(repo_name, board_name, list_name, labels)
 
 
+def cardflow(*args):
+    """
+        cardflow card user/repo [labels]
+    """
+    if len(args) < 3:
+        print(cardflow.__doc__)
+        sys.exit(1)
+
+    raise NotImplementedError()
+
+
 if __name__ == '__main__':
-    main()
+    listflow(*sys.argv[1:])
